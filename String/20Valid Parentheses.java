12b85df09bc09b4f5046506083d6bcacdbfdbdd8
cbbb24 <945911604@qq.com>
2018-07-17 13:41:04 +0800

    Create 20Valid Parentheses.java

---
+++ String/20Valid Parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s == null || s.length() == 0) return true;
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+                stack.push(s.charAt(i));
+            }else if(s.charAt(i) == ')' && !stack.empty() && stack.peek() == '('){
+                stack.pop();
+            }else if(s.charAt(i) == ']' && !stack.empty() && stack.peek() == '['){
+                stack.pop();
+            }else if(s.charAt(i) == '}' && !stack.empty() && stack.peek() == '{'){
+                stack.pop();
+            }else
+                return false;
+        }
+        return stack.empty();
+    }
+}
